19bac14ad889fb50f25d5c044233117f89edfb3f
Daniel Rosel <60182044+velocitatem@users.noreply.github.com>
2024-10-15 10:21:58 +0200

    Update routes.py

--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -4,7 +4,7 @@ from iebank_api.models import Account
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! This is only on DEV'
 
 @app.route('/skull', methods=['GET'])
 def skull():
